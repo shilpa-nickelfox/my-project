0f41296f4e289a7fe6d72cd2ac9197143f39c5ea
shilpa jha <shilpa@nickelfox.com>
2022-03-16 18:33:52 +0530

    todays work

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended : false });
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var app = express();
 
-var db = require('./api/config/db');
-dotenv.config();
+var db = require("./api/config/db");
+require("dotenv").config();
 
-var cors = require('cors'); 
+var cors = require("cors");
 app.use(cors());
 
-var routes = require('./api/routes/routes');
+var routes = require("./api/routes/routes");
 
 // app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use(urlencodedParser);
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-    res.send("Clock App");
+// app.get('/', function(req, res){
+//     res.send("Clock App");
+// });
+
+app.use("/v1", routes);
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 });
-
-app.use('/v1', routes);
-
-var server = app.listen(process.env.PORT || 3003, function(){
-    var host = server.address().address;
-    var port = server.address().port;
-})
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
@@ -38,11 +37,9 @@ app.get("*", (req, res) => {
 // if (process.env.NODE_ENV === 'production') {
 //     // Set static folder
 //     app.use(express.static('client/build'));
-  
+
 //     app.get('*', (req, res) => {
 //       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 //     });
 //   }
 console.log("Server is running");
-
-
